61a50fe592daf081d9b5537381bbaad116a05896
afex <afex@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2007-04-04 03:42:55 +0000

    Updated installation instructions.
    git-svn-id: https://plugins.svn.wordpress.org/the-hackers-diet/trunk@10163 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- hackersdiet.php
+++ hackersdiet.php
@@ -3,11 +3,11 @@
 Plugin Name: The Hacker's Diet
 Plugin URI: http://www.afex2win.com/stuff/hackersdiet/
 Description: Track your weight loss online via your blog.  Inspired by John Walker's <a href="http://www.fourmilab.ch/hackdiet/">The Hacker's Diet</a>.  This plugin replaces the Excel files supplied for his original system.
-Version: 0.9.4b
+Version: 0.9.5b
 Author: Keith 'afex' Thornhill
 Author URI: http://www.afex2win.com/
 */
-$hd_version = "0.9.4b";
+$hd_version = "0.9.5b";
 $hackdiet_user_level = 1;	// feel free to change this value (1-10) if you want to restrict lower users from using the plugin
 
 
